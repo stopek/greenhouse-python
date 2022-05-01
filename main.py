74b5d30b8af7ba2a0d7781c9c9e308ba8772eeb6
stopek <stopek.pawel@gmail.com>
2022-05-01 14:06:03 +0200

    fix: added main file

---
+++ main.py
@@ -0,0 +1,9 @@
+from boot import boot
+
+def main():
+    print("In the jungle, welcome to the jungle")
+    print("Watch it bring you to your shananananana knees, knees")
+
+
+if __name__ == '__main__':
+    main()

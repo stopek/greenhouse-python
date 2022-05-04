e291fc82d41e2e9b3894bdfb39f582a7901a417d
stopek <stopek.pawel@gmail.com>
2022-05-04 12:20:06 +0200

    feat: added the function to change wavelength to rgb

--- classes/Color.py
+++ classes/Color.py
@@ -23,3 +23,54 @@ def hex_to_rgb(hex_string):
         b_hex = hex_string[4:6]
 
     return int(r_hex, 16), int(g_hex, 16), int(b_hex, 16)
+
+
+def wavelength_to_rgb(wavelength, gamma=0.80, intensity_max=255):
+    factor = None
+    red = None
+    green = None
+    blue = None
+
+    if 380 <= wavelength < 440:
+        red = (-wavelength - 440) / (440 - 380)
+        green = 0.0
+        blue = 1.0
+    elif 440 <= wavelength < 490:
+        red = 0.0
+        green = (wavelength - 440) / (490 - 440)
+    elif 490 <= wavelength < 510:
+        red = 0.0
+        green = 1.0
+        blue = -(wavelength - 510) / (510 - 490)
+    elif 510 <= wavelength < 580:
+        red = (wavelength - 510) / (580 - 510)
+        green = 1.0
+        blue = 0.0
+    elif 580 <= wavelength < 645:
+        red = 1.0
+        green = -(wavelength - 645) / (645 - 580)
+        blue = 0.0
+    elif 645 <= wavelength < 781:
+        red = 1.0
+        green = 0.0
+        blue = 0.0
+    else:
+        red = 0.0
+        green = 0.0
+        blue = 0.0
+
+    if 380 <= wavelength < 420:
+        factor = 0.3 + 0.7 * (wavelength - 380) / (420 - 380)
+    elif 420 <= wavelength < 701:
+        factor = 1.0
+    elif 701 <= wavelength < 781:
+        factor = 0.3 + 0.7 * (780 - wavelength) / (780 - 700)
+    else:
+        factor = 0.0
+
+    rgb = []
+    rgb[0] = round(intensity_max * pow(red * factor, gamma)) if red != 0.00 else 0
+    rgb[1] = round(intensity_max * pow(green * factor, gamma)) if green != 0.00 else 0
+    rgb[2] = round(intensity_max * pow(blue * factor, gamma)) if blue != 0.00 else 0
+
+    return rgb

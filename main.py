ea9206fb08e641028481e5f1783981f8075342c8
stopek <stopek.pawel@gmail.com>
2022-05-06 10:05:14 +0200

    refactor: variable names and added main func

--- main.py
+++ main.py
@@ -7,28 +7,34 @@ from classes.Pixel import initialize_pixel
 from classes.Wifi import connect_to_wifi
 from config import WIFI_PASSWORD, WIFI_SSID, DATA_ENDPOINT, TOTAL_LEDS
 
-Log.line()
-Log.empty(2)
 
-segments = {
-    "green": initialize_pixel(32, TOTAL_LEDS),
-    "yellow": initialize_pixel(33, TOTAL_LEDS),
-    "black": initialize_pixel(25, TOTAL_LEDS),
-    "red": initialize_pixel(26, TOTAL_LEDS)
-}
+def main():
+    Log.line()
+    Log.empty(2)
 
-Log.line()
+    segments = {
+        "green": initialize_pixel(32, TOTAL_LEDS),
+        "yellow": initialize_pixel(33, TOTAL_LEDS),
+        "black": initialize_pixel(25, TOTAL_LEDS),
+        "red": initialize_pixel(26, TOTAL_LEDS)
+    }
 
-loader = Loader(segments)
-loader.enable(3)
-connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
-loader.disable()
+    Log.line()
 
-Log.line()
+    loader = Loader(segments)
+    loader.enable(10)
+    connect_to_wifi(WIFI_SSID, WIFI_PASSWORD)
+    loader.disable()
 
-build(segments, DATA_ENDPOINT)
-loadTimer = Timer(1)
-loadTimer.init(period=5000, mode=Timer.PERIODIC, callback=lambda t: build(segments, DATA_ENDPOINT))
+    Log.line()
 
-Log.empty(2)
-Log.line()
+    build(segments, DATA_ENDPOINT)
+    load_timer = Timer(1)
+    load_timer.init(period=3000, mode=Timer.PERIODIC, callback=lambda t: build(segments, DATA_ENDPOINT))
+
+    Log.empty(2)
+    Log.line()
+
+
+if __name__ == '__main__':
+    main()
